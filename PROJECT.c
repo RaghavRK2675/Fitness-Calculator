9a8eebef02e9a9f558cbfa9d01d2322bc1fbc341
Raghav <raghavrk2675@gmail.com>
2023-02-19 23:18:17 +0530

    fourth commit

--- PROJECT.c
+++ PROJECT.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+void credentials(char name, int age, float weight, float height, float bmi, float vo2max);
 int main()
 {
 char name;
@@ -23,8 +24,10 @@ printf("\nMeasure your minimum heart rate by pressing the first (index) finger a
 printf("\n\n(NOTE: The average minimum heart rate of a human being is in range of b/w: 60-100 beats per minute)");
 printf("\n\nEnter your minimum heart rate:");
 scanf("%f",&minimum_heart_rate);
-bmi=((weight)/(height*height));
-vo2max=15*(maximum_heart_rate/minimum_heart_rate); //vo2max
+
+bmi=((weight)/(height*height));                              //bmi formula
+
+vo2max=(15*((maximum_heart_rate/minimum_heart_rate)));       //vo2max formula
 
 
 
@@ -34,20 +37,25 @@ vo2max=15*(maximum_heart_rate/minimum_heart_rate); //vo2max
 
 ///Conditions for different BMI values for age group 2-20///
 
+if(age<=1)
+{
+    printf("BMI value cannot be concluded for an individual having age of 1 year or less");
+}
+
 if(2<=age<20)
 {
 
 if(bmi<5)
 {
-
+    
 }
-if(5<=bmi<=85)
+if(bmi>=5 && bmi<=85)
 {
-
+    
 }
-if(85<=bmi<=95)
+if(bmi>=85 && bmi<=95)
 {
-
+    
 }
 if(bmi>95)
 {
@@ -67,22 +75,36 @@ if(age>=20)
 
 if(bmi<18.5)
 {
-
+    
+    printf("Weight Status: Underweight");
 }
-if(18.5<=bmi<=24.9)
+if(bmi>=18.5 && bmi<=24.9)
 {
-
+    
+    printf("Weight Status: Healthy weight");
 }
-if(25<=bmi<=29.9)
+if(bmi>=25 && bmi<=29.9)
 {
-
+    
+    printf("Weight Status: Overweight");
 }
 if(bmi>=30)
 {
-
+    
+    printf("Weight Status: Obesity");
 }
 }
 }
 
 
 ///<---Conditions for different BMI values for age group 20 and greater-->///
+
+void credentials(char name, int age, float weight, float height, float bmi, float vo2max)
+{
+    printf("Name:%s\n",name);
+    printf("Age:%d\n",age);
+    printf("Weight:%f\n",weight);
+    printf("Height:%f\n",height);
+    printf("BMI:%f",bmi);
+    printf("VO2 max:%f",vo2max);
+}
