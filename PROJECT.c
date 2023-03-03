55a2b750457693d189661245b5b59e73ade67a4f
Raghav <raghavrk2675@gmail.com>
2023-03-04 02:06:29 +0530

    ninth commit

--- PROJECT.c
+++ PROJECT.c
@@ -1,3 +1,5 @@
+                                             ///Start of program///
+
 #include<stdio.h>
 void vo2max_calculations(char, int, float, float);
 int main()
@@ -6,38 +8,38 @@ char name[50],gender;
 int age,maximum_heart_rate;
 float bmi,weight,height,minimum_heart_rate,vo2max;
 
-printf("\t\t\t\t\t\tFitness Calculator");
+printf("  =================================================================== Fitness Calculator ===================================================================\n");
+printf("\n* Enter your Details/Credentials for Registration\n\n");
+                ///Credentials,calculations and medical details///
 
-///Credentials,calculations and medical details///
-
-printf("\nEnter your name                                          :");
+printf("\n>> Enter your name                                            : ");
 gets(name);
-printf("\nEnter your age                                           :");
+printf("\n>> Enter your age (In years)                                  : ");
 scanf("%d",&age);
-printf("\nEnter your gender (Enter 'M' for male and 'F' for female):\n");
+printf("\n>> Enter your gender (Enter 'M' for male and 'F' for female)  : ");
 gender=getche();
-printf("\nEnter your weight (in Kg only)                           :");
+printf("\n\n>> Enter your weight (In Kg only)                             : ");
 scanf("%f",&weight);
-printf("\nEnter your height (in m only)                            :");
+printf("\n>> Enter your height (In m only)                              : ");
 scanf("%f",&height);
 maximum_heart_rate=(220-age);
 
-printf("\nMeasure your minimum heart rate by pressing the first (index) finger and middle finger of your other hand on the inside of your wrist, at the base of your thumb");
-printf("\n\n(NOTE: The average minimum heart rate of a human being is in range of b/w: 60-100 beats per minute)");
-printf("\n\nEnter your minimum heart rate:");
+printf("\n>>> To measure your minimum heart rate/pulse at your wrist, place 2 fingers between the bone and the tendon over your radial artery which is located on the thumb side of your wrist. When you feel your pulse, count the number of beats in 15 seconds. Multiply this number by 4 to calculate your beats per minute");
+printf("\n\n> (NOTE: The average minimum heart rate of a human being is in range of b/w: 60-100 beats per minute)");
+printf("\n\n>> Enter your minimum heart rate: ");
 scanf("%f",&minimum_heart_rate);
 
-bmi=((weight)/(height*height));                              //bmi formula
+bmi=((weight)/(height*height));                                              //BMI formula
 
-vo2max=(15*((maximum_heart_rate/minimum_heart_rate)));       //vo2max formula
-
-
-
-///<---Credentials,calculations and medical details--->///
+vo2max=(15*((maximum_heart_rate/minimum_heart_rate)));                      //VO2max formula
 
 
 
-///Conditions for different BMI values for age group 2-20///
+                ///<---Credentials,calculations and medical details--->///
+
+
+
+                ///Conditions for different BMI values for age group of 1 & 2-20///
 
 if(age<=1)
 {
@@ -49,111 +51,147 @@ if(age>=2 && age<20)
 
 if(bmi<5)
 {
-    printf("\nBMI          :%.2f",bmi);
-    printf("\nWeight Status: Underweight");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : Underweight");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 if(bmi>=5 && bmi<=85)
 {
-    printf("\nBMI        :%.2f",bmi);
-    printf("\nWeight Status: Healthy Weight");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);   
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : Healthy Weight");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 if(bmi>=85 && bmi<=95)
 {
-    printf("\nBMI        :%.2f",bmi);
-    printf("\nWeight Status: At risk of overweight");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : At risk of overweight");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 if(bmi>95)
 {
-    printf("\nBMI        :%.2f",bmi);
-    printf("\nWeight Status: Overweight");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : Overweight");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 }
 
-///<---Conditions for different BMI values for age group 2-20-->///
+                ///<---Conditions for different BMI values for age group 1 & 2-20-->///
 
 
 
 
-///Conditions for different BMI values for age group 20 and greater///
+                ///Conditions for different BMI values for age group 20 and greater///
 
 if(age>=20)
 {
 
 if(bmi<18.5)
 {
-    printf("\nBMI        :%.2f",bmi);
-    printf("\nWeight Status: Underweight");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : Underweight");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 if(bmi>=18.5 && bmi<=24.9)
 {
-    printf("\nBMI        :%.2f",bmi);
-    printf("\nWeight Status: Healthy weight");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : Healthy weight");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 if(bmi>=25 && bmi<=29.9)
 {
-    printf("\nBMI        :%.2f",bmi);
-    printf("\nWeight Status: Overweight");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : Overweight");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 if(bmi>=30)
 {
-    printf("\nBMI        :%.2f",bmi);
-    printf("\nWeight Status: Obesity");
-    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);
+    printf("\n--> BMI              : %.2f",bmi);
+    printf("\n--> Weight Status    : Obesity");
+    vo2max_calculations(gender, maximum_heart_rate, minimum_heart_rate, vo2max);                    //Function call for displaying VO2max related information
 }
 }
 }
 
 
-///<---Conditions for different BMI values for age group 20 and greater-->///
+                ///<---Conditions for different BMI values for age group 20 and greater-->///
 
+
+
+
+
+                ///Function for calculation of VO2max and displaying VO2max related details///
 
 
 void vo2max_calculations(char sex, int max_heart_rate, float min_heart_rate, float vo2max)
 {
+
+    //Conditions for different VO2max levels for males//
+
     if(sex=='M')
     {
-        if(vo2max>=35 && vo2max<=40)
+        if(vo2max<35)
         {
-            printf("\nVO2max       : %.2f",vo2max);
-            printf("\nVO2max status: Average");
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Bad\n");
+            printf("\n~ Fitness level: Bad\n");
+        }
+
+        else if(vo2max>=35 && vo2max<=40)
+        {
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Average\n");
+            printf("\n~ Fitness level: Average\n");
         }
         else if(vo2max>=45 && vo2max<=50)
         {
-            printf("\nVO2max       : %.2f",vo2max);
-            printf("\nVO2max status: Good");
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Good\n");
+            printf("\n~ Fitness level: Good\n");
         }
         else if(vo2max>=60)
         {
-            printf("\nVO2max       : %.2f",vo2max);
-            printf("\nVO2max status: Excellent");
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Excellent\n");
+            printf("\n~ Fitness level: Excellent\n");
         }
     }
 
+    //<---Conditions for different VO2max levels for males--->//
+
+
+    //Conditions for different VO2max levels for females//
 
     if(sex=='F')
     {
-        if(vo2max>=35 && vo2max<=40)
+        if(vo2max<35)
         {
-            printf("\nVO2max       : %.2f",vo2max);
-            printf("\nVO2max status: Average");
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Bad\n");
+            printf("\n~ Fitness level: Bad\n");
+        }
+
+        else if(vo2max>=35 && vo2max<=40)
+        {
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Average\n");
+            printf("\n~ Fitness level: Average\n");
         }
         else if(vo2max>=38 && vo2max<=42)
         {
-            printf("\nVO2max       : %.2f",vo2max);
-            printf("\nVO2max status: Good");
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Good\n");
+            printf("\n~ Fitness level: Good\n");
         }
         else if(vo2max>=50)
         {
-            printf("\nVO2max       : %.2f",vo2max);
-            printf("\nVO2max status: Excellent");
+            printf("\n--> VO2max           : %.2f",vo2max);
+            printf("\n--> VO2max status    : Excellent\n");
+            printf("\n~ Fitness level: Excellent\n");
         }
     }
 
+                 //<---Conditions for different VO2max levels for females--->//
+
 }
+
+        ///<---Function for calculation of VO2max and displaying VO2max related details--->///
